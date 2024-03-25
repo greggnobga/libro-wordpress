2bb13d34e6d33dc62c289a8341d9789bbcde0765
Gregg N <greggnobga@gmail.com>
2024-03-25 18:42:49 +0800

    Add enviroment variable.

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'libro' );
+define( 'DB_NAME', $_ENV['DB_NAME']);
 
 /** Database username */
-define( 'DB_USER', 'reijo' );
+define( 'DB_USER', $_ENV['DB_USER']);
 
 /** Database password */
-define( 'DB_PASSWORD', '250MY542' );
+define( 'DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
 /** Database hostname */
-define( 'DB_HOST', 'mysqldb' );
+define( 'DB_HOST', $_ENV['DB_HOST']);
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', $_ENV['DB_CHARSET']);
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', $_ENV['DB_COLLATE']);
 
 /**#@+
  * Authentication unique keys and salts.
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'EVlhxi>;,78NxDjq$/5Xy@o?:tIbG3O,%L1vmMej{PE3oUm450=i|5v(oKz931lj' );
-define( 'SECURE_AUTH_KEY',  'Slb/4zD|8BXHtK@SJg.~@38z+O,IaB8}-s7f]9xfhwL[XTK=1J {o~_ E>AY[.Am' );
-define( 'LOGGED_IN_KEY',    'cUJ5mvDOzEAg#0h>zhee;6<prh4r-MF!2lfkqXFl`pkGp><GsvUo_~Au5E1v/H~*' );
-define( 'NONCE_KEY',        'WR=HiJtH$ ?tMX3O E29&c]Z{D5q|&PEuTKqP&km?LW0<_%0L8Fd!dnyTwVpz<pa' );
-define( 'AUTH_SALT',        ':h;WyQuP0Ki5k>R@xjgP=(M&2GVP(nax|2H9SN6$%e%R8EP^3D=:Q)&H~,0U3D0w' );
-define( 'SECURE_AUTH_SALT', 'K>F&J8#?uX+%em`W,5iWSF@6%YxlsYtSd$(azl`+F7:oy4$P(;$)yYj*eB*i(Ei@' );
-define( 'LOGGED_IN_SALT',   'Azs]Ni*`#A|@Pbs4TxE+](xmLv{=M#>Y>FRjI?C@Hv=_*O]Onujnjbm}v[qMKQs)' );
-define( 'NONCE_SALT',       'xIaz5<(.xcIFSAzKOB]|st_IHlJlDf_Q5T0!;W ]eE`$[YhbE$N^|_gJ%va]6m[5' );
+define( 'AUTH_KEY',         $_ENV['AUTH_KEY']);
+define( 'SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
+define( 'LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
+define( 'NONCE_KEY',        $_ENV['NONCE_KEY']);
+define( 'AUTH_SALT',        $_ENV['AUTH_SALT']);
+define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define( 'LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
+define( 'NONCE_SALT',       $_ENV['NONCE_SALT']);
 
 /**#@-*/
 
@@ -82,8 +82,6 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
-
 
 /* That's all, stop editing! Happy publishing. */
 
