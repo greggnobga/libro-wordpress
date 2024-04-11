7b4d6c0a2abdfcb77038376a74a7828ce09f6d02
Gregg N <greggnobga@gmail.com>
2024-04-11 23:35:35 +0800

    Add card.

--- wp-content/themes/libro/books.php
+++ wp-content/themes/libro/books.php
@@ -3,11 +3,47 @@
 <?php get_header(); ?>
 
 <!-- Section -->
-<section class="min-h-screen bg-white dark:bg-gray-700">
+<section class="min-h-screen bg-gray-700">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-         <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-4">Custom Page Template For Books</h1>
-            <p class="text-white">Content here..</p>
+         <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+            <!-- Heading -->
+            <h1 class="text-white text-3xl md:text-5xl font-extrabold mb-4">Books</h1>
+
+            <!-- Card -->
+             <div class="grid grid-cols-1 md:grid-cols-4 pb-4">
+                <div class="col-span-1 aspect-w-16 aspect-h-9"><img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel.svg" alt="" /></div>
+                <div class="col-span-3 border border-gray-700 p-2">
+                    <h2 class="text-white text-2xl md:text-3xl font-extrabold mb-4">Title</h2>
+                    <p class="text-gray-300 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                        Read more
+                    </a>
+                </div>
+             </div>
+
+              <!-- Card -->
+             <div class="grid grid-cols-1 md:grid-cols-4 pb-4">
+                <div class="col-span-1 aspect-w-16 aspect-h-9"><img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel.svg" alt="" /></div>
+                <div class="col-span-3 border border-gray-700 p-2">
+                    <h2 class="text-white text-2xl md:text-3xl font-extrabold mb-4">Title</h2>
+                    <p class="text-gray-300 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                        Read more
+                    </a>
+                </div>
+             </div>
+
+              <!-- Card -->
+             <div class="grid grid-cols-1 md:grid-cols-4 pb-4">
+                <div class="col-span-1 aspect-w-16 aspect-h-9"><img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel.svg" alt="" /></div>
+                <div class="col-span-3 border border-gray-700 p-2">
+                    <h2 class="text-white text-2xl md:text-3xl font-extrabold mb-4">Title</h2>
+                    <p class="text-gray-300 pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                        Read more
+                    </a>
+                </div>
+             </div>
         </div>
     </div>
 </section>
--- wp-content/themes/libro/index.php
+++ wp-content/themes/libro/index.php
@@ -35,16 +35,16 @@
     </div>
     <!-- Slider controls -->
     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
             </svg>
             <span class="sr-only">Previous</span>
         </span>
     </button>
     <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
             <span class="sr-only">Next</span>
@@ -54,35 +54,35 @@
 
 
 <!-- Section -->
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-        <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">Go on an adventure with completely broken heroes.</h1>
-            <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <a href="/books" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+            <h1 class="text-white text-3xl md:text-5xl font-extrabold mb-2">Go on an adventure with completely broken heroes.</h1>
+            <p class="text-lg font-normal text-gray-400 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="/books" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                 Read Books
             </a>
         </div>
         <div class="grid md:grid-cols-2 gap-8">
-            <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
-                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Check in for your next journey, for thy schedule is tight.</h2>
-                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="/calendar" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Release Schedule</a>
+            <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12">
+                <h2 class="text-white text-3xl font-extrabold mb-2">Take a look at us; we are happy to get acquainted with you better.</h2>
+                <p class="text-lg font-normal text-gray-400 mb-4">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="/about" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Release Schedule</a>
             </div>
-            <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
-                <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">If you can't find what you're looking for, get in touch with us.</h2>
-                <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <a href="/contact" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Contact Us</a>
+            <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12">
+                <h2 class="text-white text-3xl font-extrabold mb-2">If you can't find what you're looking for, get in touch with us.</h2>
+                <p class="text-lg font-normal text-gray-400 mb-4">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <a href="/contact" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Contact Us</a>
             </div>
         </div>
     </div>
 </section>
 
 <!-- Section -->
-<section class="bg-white dark:bg-gray-700">
+<section class="bg-gray-800">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-         <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <h1 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-4">Latest Published Books</h1>
+         <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+            <h1 class="text-white text-3xl md:text-5xl font-extrabold mb-4">Latest Published Books</h1>
             <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
             <?php
                 $latest_books = new WP_Query(array(
@@ -93,22 +93,22 @@
                     while($latest_books->have_posts()) {
                         $latest_books->the_post(); ?>
                         <!-- Card -->
-                        <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="w-full rounded-lg shadow bg-gray-900 border-gray-700">
                             <a href="<?php the_permalink(); ?>">
                                 <img class="rounded-t-lg" src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel.svg" alt="" />
                             </a>
                             <div class="p-5">
                                 <a href="<?php the_permalink(); ?>">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo wp_trim_words( get_the_title(), 5) ?></h5>
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white"><?php echo wp_trim_words( get_the_title(), 5) ?></h5>
                                 </a>
-                                <div class="mb-3 font-normal text-gray-700 dark:text-gray-400"><?php echo wp_trim_words( get_the_content(), 25) ?></div>
-                                <a href="<?php the_permalink(); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <div class="mb-3 font-normal text-gray-400"><?php echo wp_trim_words( get_the_content(), 25) ?></div>
+                                <a href="<?php the_permalink(); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                                     Read more
                                 </a>
                             </div>
                         </div>
                 <?php } } else { ?>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">No books published.</p>
+                        <p class="mb-3 font-normal text-gray-400">No books published.</p>
                <?php } ?>
             </div>
         </div>
@@ -116,15 +116,15 @@
 </section>
 
 <!-- Section -->
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Looking for a web developer?</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
-        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">Looking for a web developer?</h1>
+        <p class="mb-8 text-lg font-normal  lg:text-xl sm:px-16 lg:px-48 text-gray-400">Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+        <div class="flex flex-col gap-2 space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                 Contact Us
             </a>
-            <a href="#" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70">
+            <a href="#" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 bg-gray-800 text-gray-400 border-gray-700 hover:text-gray-200 hover:bg-gray-700">
                 Our Portfolio
             </a>
         </div>
