253f54c9aaa22bc3ff4a7ce0520f2a267e4f27bd
Gregg N <greggnobga@gmail.com>
2024-04-13 23:30:44 +0800

    Add sprite icon.

--- wp-content/themes/libro/header.php
+++ wp-content/themes/libro/header.php
@@ -5,12 +5,13 @@
         <?php wp_head(); ?>
     </head>
     <body>
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <nav class="border border-gray-700 bg-gray-900">
             <div class="flex flex-wrap items-center justify-between p-4">
-                    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span class="self-center uppercase text-2xl font-semibold whitespace-nowrap dark:text-white">Libro</span>
+                 <a href="/" class="flex items-center space-x-2 rtl:space-x-reverse">
+                    <svg class="w-6 h-6" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#book"></use></svg>
+                    <span class="self-center uppercase text-2xl font-semibold whitespace-nowrap text-gray-200">Libro</span>
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span class='hamburger'>
                         <span class='hamburger-box'>
                             <span class='hamburger-inner'></span>
@@ -18,21 +19,26 @@
                     </span>
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                             <a href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
+                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-transparent border-gray-700">
+                        <li class="flex flex-row gap-2 py-2 md:p-0">
+                            <svg class="w-5 h-5" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#home"></use></svg>
+                            <a href="/" class="rounded md:border-0 md:p-0 text-gray-200 md:hover:text-blue-500 hover:bg-gray-700 hover:text-gray-200 md:hover:bg-transparent">Home</a>
                         </li>
-                        <li>
-                            <a href="/books" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Books</a>
+                        <li class="flex flex-row gap-2 py-2 md:p-0">
+                            <svg class="w-5 h-5" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#book"></use></svg>
+                            <a href="/books" class="rounded md:border-0 md:p-0 text-gray-200 md:hover:text-blue-500 hover:bg-gray-700 hover:text-gray-200 md:hover:bg-transparent">Books</a>
                         </li>
-                        <li>
-                            <a href="/blog" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                        <li class="flex flex-row gap-2 py-2 md:p-0">
+                            <svg class="w-5 h-5" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#blog"></use></svg>
+                            <a href="/blog" class="rounded md:border-0 md:p-0 text-gray-200 md:hover:text-blue-500 hover:bg-gray-700 hover:text-gray-200 md:hover:bg-transparent">Blog</a>
                         </li>
-                        <li>
-                            <a href="/about" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                        <li class="flex flex-row gap-2 py-2 md:p-0">
+                            <svg class="w-5 h-5" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#about"></use></svg>
+                            <a href="/about" class="rounded md:border-0 md:p-0 text-gray-200 md:hover:text-blue-500 hover:bg-gray-700 hover:text-gray-200 md:hover:bg-transparent">About</a>
                         </li>
-                        <li>
-                            <a href="/contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        <li class="flex flex-row gap-2 py-2 md:p-0">
+                            <svg class="w-5 h-5" fill="white"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#contact"></use></svg>
+                            <a href="/contact" class="rounded md:border-0 md:p-0 text-gray-200 md:hover:text-blue-500 hover:bg-gray-700 hover:text-gray-200 md:hover:bg-transparent">Contact</a>
                         </li>
                     </ul>
                 </div>
