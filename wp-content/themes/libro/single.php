d226105ef8521580033b821fa33104d06b19d740
Gregg N <greggnobga@gmail.com>
2024-04-29 00:01:18 +0800

    Update single post frontend.

--- wp-content/themes/libro/single.php
+++ wp-content/themes/libro/single.php
@@ -3,10 +3,24 @@
 <!-- Section -->
 <section class="min-h-screen bg-gray-700">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-         <div class="bg-gray-800 border border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <h1 class="text-gray-200 text-3xl md:text-5xl font-extrabold mb-4"><?php the_title(); ?></h1>
-            <div class="text-white">
-                <?php the_content(); ?>
+         <!-- Wrapper -->
+         <div class="bg-gray-800 rounded-lg p-8 md:p-12 mb-8">
+            <!-- Card -->
+            <div class="grid grid-cols-1 md:grid-cols-4">
+                <div class="col-span-1 aspect-w-16 aspect-h-9 p-2">
+                    <img class="pb-2 h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri(); ?>/assets/images/carousel.svg" alt="" />
+                    <p class="py-1 font-thin text-gray-400"><?php the_category(); ?></p>
+                    <p class="py-1 font-thin text-gray-400"><?php the_tags(); ?></p>
+                    <p class="py-1 font-thin text-gray-400">Posted: <?php $post_date = get_the_date( 'l F j, Y' ); echo $post_date; ?></p>
+                </div>
+                <div class="col-span-3 p-2">
+                    <a href="#">
+                        <h5 class="pb-2 mb-2 text-2xl font-bold tracking-tight text-white border-b border-gray-700"><?php the_title(); ?></h5>
+                    </a>
+                    <div class="mb-3 font-normal text-gray-400">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
